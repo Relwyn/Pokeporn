2dba00e32cb520d2da2a5a2e413475a0627ad753
Relwyn <maerten.nicolas62@gmail.com>
2019-04-03 17:02:39 +0200

    init firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,17 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCejaK8XdI-d2Dyfdgiduygmn7npY4MLsk',
+  authDomain: 'pokeporn-2650c.firebaseapp.com',
+  databaseURL: 'https://pokeporn-2650c.firebaseio.com',
+  projectId: 'pokeporn-2650c',
+  storageBucket: 'pokeporn-2650c.appspot.com',
+  messagingSenderId: '570977573792'
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -15,6 +26,7 @@ export class AppComponent {
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    firebase.initializeApp(config);
   }
 
   initializeApp() {
