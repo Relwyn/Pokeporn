c9247c732a5d213aad777a4365b488d67e9be563
helori <helorigouin@gmail.com>
2019-04-03 14:52:50 +0200

    List of pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,30 +1,41 @@
 import { Component } from '@angular/core'
 import { Router } from '@angular/router'
-import { PokeApiService } from "../poke-api.service";
+import { PokeApiService } from '../poke-api.service'
 
-//import { IonicStorageModule } from "@ionic/storage";
+//import { IonicStorageModule } from "@ionic/storage"
+
+class Pokemon {
+    name: string
+    url: string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
 export class HomePage {
 
     constructor (
         private router: Router,
         private pokeApiService: PokeApiService
-    )
-    {
+    ) {
         this.getPokemons()
     }
 
+    pokemons: Pokemon[] = []
+
     getPokemons() {
 
+        let self = this
+
       this.pokeApiService.getPokemons().subscribe((val) => {
-          let result: any = val
-          console.log(result)
+
+          val.results.forEach(function(result) {
+              self.pokemons.push(result)
+          })
+
+          console.log(this.pokemons)
       })
     }
 
--- src/app/poke-api.service.ts
+++ src/app/poke-api.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PokeApiService {
 
-  private allPokemonsUrl: string = "http://pokeapi.co/api/v2/pokemon/"
+  private allPokemonsUrl: string = "http://pokeapi.co/api/v2/pokemon/?offset=0&limit=100"
 
   constructor(private http: HttpClient) { }
 
