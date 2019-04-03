4af01a4cb9cf2ce3130a7d3a107bafaf72f9ea3c
Helori <helori@David-Hasselhoff.local>
2019-04-03 14:25:08 +0200

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+import { PokeApiService } from "../poke-api.service";
+import {TransferDataService} from "../../../../travel/src/app/transfer-data.service";
+
+//import { IonicStorageModule } from "@ionic/storage";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+    constructor (
+        private router: Router,
+        private TransferDataService: TransferDataService
+    ) {}
+
+    
 
 }
