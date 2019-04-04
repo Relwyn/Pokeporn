3fed0bd6d4234ad27f99bf2a4685bb08fa42dc99
helori <helorigouin@gmail.com>
2019-04-04 14:27:45 +0200

    feature: Pokemon team

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,12 +3,12 @@ import { Router } from '@angular/router'
 import { PokeApiService } from '../poke-api.service'
 import { AlertController } from '@ionic/angular'
 import { Storage } from '@ionic/storage'
-import { ToastController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular'
+
 class Pokemon {
-    id: number;
-    name: string;
-    url: string;
-    favorite: boolean;
+    id: number
+    name: string
+    url: string
 }
 
 @Component({
@@ -21,27 +21,12 @@ export class HomePage {
     constructor (
         private router: Router,
         private pokeApiService: PokeApiService,
-        private modal: AlertController,
+        private alert: AlertController,
         private localStorage: Storage,
-        private toast: ToastController,
+        private toast: ToastController
     ) {
         this.getPokemons()
-    }
-
-    async showToastError(error) {
-        const toast = await this.toast.create({
-            message: `${error}`,
-            duration: 10000,
-            showCloseButton: true,
-            position: 'top',
-            closeButtonText: 'Done'
-        });
-
-        toast.present()
-    }
-
-    list_profil_page(){
-        this.router.navigate(['/list-profil'])
+        this.getTeam()
     }
 
     inputSearch: string
@@ -54,7 +39,23 @@ export class HomePage {
 
         this.localStorage.get('pokemons').then((favoritePokemons) => {
 
-            self.pokemons = favoritePokemons
+            if (favoritePokemons)
+                self.pokemons = favoritePokemons
+            else
+                self.pokemons = []
+        })
+    }
+
+    getTeam() {
+
+        const self = this
+
+        this.localStorage.get('team').then((teamPokemons) => {
+
+            if (teamPokemons)
+                self.team = teamPokemons
+            else
+                self.team = []
         })
     }
 
@@ -76,10 +77,11 @@ export class HomePage {
 
     addToTeam(pokemon) {
 
-        if (this.team.length < 6)
+        if (this.team.length < 6) {
             this.team.push(pokemon)
-        //else
-
+            this.showToast('Successfully added to team')
+        } else
+            this.showToast('Your team is full')
 
         this.saveItemInLocalStorage('team', this.team)
     }
@@ -94,42 +96,41 @@ export class HomePage {
             return pokemon
         }, error => {
             console.log(error)
-            this.showToastError(JSON.stringify(error))
+            this.showToast(JSON.stringify(error))
         })
     }
 
     async openPokedex(pokemon) {
 
-        const self = this
-
-        const alert = await this.modal.create({
+        const alert = await this.alert.create({
             header: pokemon.name + '   n° ' + pokemon.id,
             subHeader: this.typesIntoString(pokemon),
-            message: '<ion-list>' +
-                '<ion-item>' +
-                '<img src="' + pokemon.sprites.front_default + '">' +
-                '</ion-item>' +
-                '<ion-item>' +
-                '<p>Weight: ' + pokemon.weight + 'kg</p>' +
-                '</ion-item>' +
-                '<ion-item>' +
-                '<p>hp: ' + pokemon.stats[5].base_stat + '</p>' +
-                '</ion-item>' +
-                '<ion-item>' +
-                '<p>attack: ' + pokemon.stats[4].base_stat + '</p>' +
-                '</ion-item>' +
-                '<ion-item>' +
-                '<p>defense: ' + pokemon.stats[3].base_stat + '</p>' +
-                '</ion-item>' +
-                '<ion-item>' +
-                '<p>speed: ' + pokemon.stats[0].base_stat + '</p>' +
-                '</ion-item>' +
-                '<ion-item>' +
-                '<p>special-attack: ' + pokemon.stats[2].base_stat + '</p>' +
-                '</ion-item>' +
-                '<ion-item>' +
-                '<p>special-defense: ' + pokemon.stats[1].base_stat + '</p>' +
-                '</ion-item>' +
+            message:
+                '<ion-list>' +
+                    '<ion-item>' +
+                        '<img src="' + pokemon.sprites.front_default + '">' +
+                    '</ion-item>' +
+                    '<ion-item>' +
+                        '<p>Weight: ' + pokemon.weight + 'kg</p>' +
+                    '</ion-item>' +
+                    '<ion-item>' +
+                        '<p>hp: ' + pokemon.stats[5].base_stat + '</p>' +
+                    '</ion-item>' +
+                    '<ion-item>' +
+                        '<p>attack: ' + pokemon.stats[4].base_stat + '</p>' +
+                    '</ion-item>' +
+                    '<ion-item>' +
+                        '<p>defense: ' + pokemon.stats[3].base_stat + '</p>' +
+                    '</ion-item>' +
+                    '<ion-item>' +
+                        '<p>speed: ' + pokemon.stats[0].base_stat + '</p>' +
+                    '</ion-item>' +
+                    '<ion-item>' +
+                        '<p>special-attack: ' + pokemon.stats[2].base_stat + '</p>' +
+                    '</ion-item>' +
+                    '<ion-item>' +
+                        '<p>special-defense: ' + pokemon.stats[1].base_stat + '</p>' +
+                    '</ion-item>' +
                 '</ion-list>',
             buttons: [
                 {
@@ -141,13 +142,73 @@ export class HomePage {
                     text: 'Save',
                     cssClass: 'primary',
                     handler: () => {
-                        self.savePokemon(pokemon)
+                        this.savePokemon(pokemon)
                     }
                 }
             ]
-        });
+        })
+
+        await alert.present()
+    }
+
+    async openTeamScreen(pokemon = false) {
+
+        let message: string =
+            '<ion-grid>' +
+                '<ion-row>'
+
+        this.team.forEach(function(pokemon, index) {
+
+            message+=
+                '<ion-col>' +
+                    '<ion-row>' +
+                        '<span>' + pokemon.name + '</span>' +
+                    '</ion-row>' +
+                '<ion-row>' +
+                    '<img src="' + pokemon.sprites.front_default + '">' +
+                '</ion-row>' +
+                    '<ion-row>' +
+                        '<ion-button id="teamRemoveButton">Remove</ion-button>' +
+                    '</ion-row>' +
+                '</ion-col>'
+
+        })
+
+        message+=
+                '</ion-row>' +
+            '</ion-grid>'
+
+        console.log(message)
+
+        const alert = await this.alert.create({
+            header: 'Team',
+            message: message,
+            buttons: [
+                {
+                    text: 'Close',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                }
+            ]
+        })
 
         await alert.present();
+    }
+
+    async showToast(message) {
+        const toast = await this.toast.create({
+            message: `${message}`,
+            duration: 10000,
+            showCloseButton: true,
+            position: 'top',
+            closeButtonText: 'Done'
+        })
+
+        toast.present()
+    }
+
+    list_profil_page(){
+        this.router.navigate(['/list-profil'])
     }
 
     saveItemInLocalStorage(name, item) {
