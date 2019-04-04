6f4530bbd848b0a1fa6076029c5ddb98ad44e147
helori <helorigouin@gmail.com>
2019-04-04 11:08:11 +0200

    feature: Review of all pokemon list system

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router'
 import { PokeApiService } from '../poke-api.service'
 import { AlertController } from '@ionic/angular'
 import { Storage } from '@ionic/storage'
+import {calculateSizes} from "@angular-devkit/build-angular/src/angular-cli-files/utilities/bundle-calculator";
 
 class Pokemon {
     id: number;
@@ -29,109 +30,126 @@ export class HomePage {
 
     inputSearch: string
     pokemons: Pokemon[] = []
-    favoritePokemonsSaved: Pokemon[] = []
+    team: Pokemon[] = []
 
     getPokemons() {
 
         const self = this
 
-      this.pokeApiService.getPokemons().subscribe((val) => {
+        this.localStorage.get('pokemons').then((favoritePokemons) => {
 
-          const res: any = val
-
-          res.results.forEach(function(element, index) {
-              element.id = index
-
-              self.localStorage.get('favoritePokemons').then((favoritePokemons) => {
-
-                  self.favoritePokemonsSaved = favoritePokemons
-
-                  self.favoritePokemonsSaved.forEach(function(favoritePokemon) {
-
-                      if(favoritePokemon.name === element.name)
-                          element.favorite = true
-
-                  })
-              })
-
-              self.pokemons.push(element)
-          })
-      })
-    }
-
-    setPokemonAsFavorite(id) {
-
-        let index = this.getIndexWithId(this.pokemons, id)
-
-        this.pokemons[index].favorite = !this.pokemons[index].favorite
-
-        if (this.pokemons[index].favorite)
-            this.favoritePokemonsSaved.push(this.pokemons[index])
-        else
-            this.favoritePokemonsSaved.splice(this.getIndexWithId(this.favoritePokemonsSaved, id), 1)
-
-        console.log(this.favoritePokemonsSaved)
-
-        this.pushItemInLocalStorage('favoritePokemons', this.favoritePokemonsSaved)
-    }
-
-    pushItemInLocalStorage(name, item) {
-        this.localStorage.set(name, item)
-    }
-
-    getIndexWithId(array, id) {
-        return array.findIndex(item => item.id === id)
-    }
-
-    searchByName(name = this.inputSearch) {
-
-        this.pokeApiService.getPokemonByName(name).subscribe((val) => {
-            this.openPokedex(val)
+            self.pokemons = favoritePokemons
         })
     }
 
-    async openPokedex(data) {
+    savePokemon(pokemon) {
+
+        this.pokemons.push(pokemon)
+
+        this.saveItemInLocalStorage('pokemons', this.pokemons)
+    }
+
+    removePokemon(pokemon) {
+
+        let index = this.getIndexWithId(this.pokemons, pokemon.id)
+
+        this.pokemons.splice(index, 1)
+
+        this.saveItemInLocalStorage('pokemons', this.pokemons)
+    }
+
+    addToTeam(pokemon) {
+
+        if (this.team.length < 6)
+            this.team.push(pokemon)
+        //else
+
+
+        this.saveItemInLocalStorage('team', this.team)
+    }
+
+    async searchByName(name = this.inputSearch, openPokedex = true) {
+
+        this.pokeApiService.getPokemonByName(name).subscribe((pokemon) => {
+
+            if(openPokedex)
+                this.openPokedex(pokemon)
+
+            return pokemon
+        })
+    }
+
+    async openPokedex(pokemon) {
+
+        const self = this
 
         const alert = await this.modal.create({
-            header: data.name + '   n° ' + data.id,
-            subHeader: this.typesIntoString(data),
+            header: pokemon.name + '   n° ' + pokemon.id,
+            subHeader: this.typesIntoString(pokemon),
             message: '<ion-list>' +
                 '<ion-item>' +
-                '<img src="' + data.sprites.front_default + '">' +
+                '<img src="' + pokemon.sprites.front_default + '">' +
                 '</ion-item>' +
                 '<ion-item>' +
-                '<p>Weight: ' + data.weight + 'kg</p>' +
+                '<p>Weight: ' + pokemon.weight + 'kg</p>' +
                 '</ion-item>' +
                 '<ion-item>' +
-                '<p>hp: ' + data.stats[5].base_stat + '</p>' +
+                '<p>hp: ' + pokemon.stats[5].base_stat + '</p>' +
                 '</ion-item>' +
                 '<ion-item>' +
-                '<p>attack: ' + data.stats[4].base_stat + '</p>' +
+                '<p>attack: ' + pokemon.stats[4].base_stat + '</p>' +
                 '</ion-item>' +
                 '<ion-item>' +
-                '<p>defense: ' + data.stats[3].base_stat + '</p>' +
+                '<p>defense: ' + pokemon.stats[3].base_stat + '</p>' +
                 '</ion-item>' +
                 '<ion-item>' +
-                '<p>speed: ' + data.stats[0].base_stat + '</p>' +
+                '<p>speed: ' + pokemon.stats[0].base_stat + '</p>' +
                 '</ion-item>' +
                 '<ion-item>' +
-                '<p>special-attack: ' + data.stats[2].base_stat + '</p>' +
+                '<p>special-attack: ' + pokemon.stats[2].base_stat + '</p>' +
                 '</ion-item>' +
                 '<ion-item>' +
-                '<p>special-defense: ' + data.stats[1].base_stat + '</p>' +
+                '<p>special-defense: ' + pokemon.stats[1].base_stat + '</p>' +
                 '</ion-item>' +
                 '</ion-list>',
-            buttons: ['OK']
+            buttons: [
+                {
+                    text: 'Close',
+                    role: 'cancel',
+                    cssClass: 'secondary'
+                },
+                {
+                    text: 'Save',
+                    cssClass: 'primary',
+                    handler: () => {
+                        self.savePokemon(pokemon)
+                    }
+                }
+            ]
         });
 
         await alert.present();
     }
 
-    typesIntoString(data) {
+    saveItemInLocalStorage(name, item) {
+        this.localStorage.set(name, item)
+    }
+
+    getIndexWithId(array, id) {
+
+        let index = array.findIndex(item => item.id === id)
+
+        if (index === -1)
+            return false
+        else
+            return index
+    }
+
+    typesIntoString(pokemon) {
 
         let types: string = ""
 
-        data.types.forEach(function(element) {
+        pokemon.types.forEach(function(element) {
             types += element.type.name + ', '
         })
 
@@ -139,5 +157,4 @@ export class HomePage {
 
         return types
     }
-
 }
